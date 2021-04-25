ee96e55bba5086d35a65eab3e09f9b0b8de0949a
ziyuanbaoku <814228604@qq.com>
2021-04-25 22:49:46 +0800

    新手课程列表更新

--- js/index.js
+++ js/index.js
@@ -32,18 +32,61 @@ $.each(course_list_div,function(index,value){
 
 
 /*======================================【新手课程列表】==================================*/
-var thumbnail = $('.thumbnail');
-var newsImg = $('.thumbnail .img_mian img');
 
-$.each(thumbnail,function(index,value){
+//获取新手课程列表【容器】
+var newke = $('.newke');
+//获取新手课程列表【数据】
+var newkeData;
 
-	$(this).hover(function(){
-       newsImg.eq(index).css('transform','scale(1.2)');
-       $(this).css('box-shadow','0px 0px 10px red');
-	},function(){
-      newsImg.eq(index).css('transform','scale(1)');
-      $(this).css('box-shadow','none');
+//调取【新手课程】对应的【JSON数据】
+$.getJSON('data/newke.json',function(res,status,xhr){
+	newkeData = res;
+	
+	//通过数据循环列表
+	$.each(newkeData,function(index,value){
+	 
+	    //存储【新手课程】的【HTML布局代码】
+	    var str='';
+	    for(var i=0;i<newkeData.length;i++){
+	    	str += `<div class="col-sm-4 col-md-3">
+	    			       <div class="thumbnail">
+	    			         <div class="img_mian">
+	    			            <img class="img-responsive" src="${newkeData[i].url}" alt="...">
+	    			         </div>                  
+	    			         <div class="caption">
+	    			           <h3 class="title">${newkeData[i].title}</h3>
+	    			           <p><a href="#" class="btn btn-primary" role="button">购买课程</a> <a href="#" class="btn btn-default" role="button">课程详情</a></p>
+	    			         </div>
+	    			       </div>
+	    			     </div>`;
+	    }
+	    //将遍历出来的代码，添加到容器中
+	    newke.html('<div class="row">' + str + '</div>');
+	
+	
 	});
+	
+	
+	//对【新手课程列表】做CSS特效样式
+	var thumbnail = $('.thumbnail');
+	var newsImg = $('.thumbnail .img_mian img');
+	
+	
+	
+	$.each(thumbnail,function(index,value){
+	
+		$(this).hover(function(){
+	       newsImg.eq(index).css('transform','scale(1.2)');
+	       $(this).css('box-shadow','0px 0px 10px red');
+		},function(){
+	      newsImg.eq(index).css('transform','scale(1)');
+	      $(this).css('box-shadow','none');
+		});
+	});
+	
+	
+	
+	
 });
 
 
@@ -106,16 +149,32 @@ $.each(thumbnail,function(index,value){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
+
+
+
+
+// function newke(data){
+// 	//设定存储【新手课程】布局代码
+// 	var str;
+// 	for(var i=0;i<data.length;i++){
+// 		str += `<div class="col-sm-4 col-md-3">
+// 				       <div class="thumbnail">
+// 				         <div class="img_mian">
+// 				            <img class="img-responsive" src="${data[i].url}" alt="...">
+// 				         </div>                  
+// 				         <div class="caption">
+// 				           <h3 class="title">${data[i].title}</h3>
+// 				           <p><a href="#" class="btn btn-primary" role="button">购买课程</a> <a href="#" class="btn btn-default" role="button">课程详情</a></p>
+// 				         </div>
+// 				       </div>
+// 				     </div>`;
+// 	}
+	
+// 	console.log(str);
+		
+// }
+
+
+
